ce1ca09c28195f859a53b1a013ac5de3147c1264
Yuhe Ma <yuhem@andrew.cmu.edu>
2024-05-18 03:36:38 -0400

    Create variables.js

---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+/*
+It's good practice to always declare variables before use
+var (1995-2015)
+let and const (2015-present)
+*/
+
+var js_varname_is_CaSE_Sensitive = "Only use var when MUST support old browers";
+let _underscore = "Only use let when cannot use const";
+const $dollarSign = "Used when value or type will not change";
+
+/* Naming convention
+camelCase for variables, functions, object properties
+UPPER_WITH_UNDERSCORE for const
+PascalCase for classes
+kebab-case.js for file names
+$button for jQuery
+*/
+
+
